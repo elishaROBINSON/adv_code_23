0242ba64f820f9d3416165ae7bc09770c3c18854
elisha.rob <elisha.rob@gmail.com>
2023-12-04 12:43:42 +0400

    finished day 1 :)

--- day1_p1.pl
+++ day1_p1.pl
@@ -5,23 +5,9 @@ use warnings;
 open(my $file, '<', 'file.txt'); 
 
 my $sum = 0;
-my %val_hash = {
-one => 1,
-two => 2,
-three => 3,
-four => 4,
-five => 5,
-six => 6,
-seven => 7,
-eight => 8,
-nine => 9
-};
 while (my $line = <$file>) {
     chomp $line;  
-    my @digit = grep { $_ =~ /\d+|one|two|three|four|five|six|seven|eight|nine/ } split //, $line; 
-    
-    
-    
+    my @digit = grep { $_ =~ /\d+/ } split //, $line; 
     if (@digit) {
 	    if (scalar(@digit) > 1) 
 	    {
@@ -35,4 +21,3 @@ while (my $line = <$file>) {
 }
 close($file);
 print $sum;
-
--- day1_p2.pl
+++ day1_p2.pl
@@ -6,24 +6,27 @@ open(my $file, '<', 'file.txt');
 
 my $sum = 0;
 use Data::Dumper;
-my %val_store = ("one",1,"two",2,"three",3,"four",4,"five",5,"six",6,"seven",7,"eight",8,"nine",9);
+my %val_store =(
+    'one' => 1, 'two' => 2, 'three' => 3, 'four' => 4,
+    'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9
+); 
 
 while (my $line = <$file>) {
-    chomp $line;  
-    my @digit = $line =~/\b(one|two|three|four|five|six|seven|eight|nine)\b|\d/g; 
-    print "--->".Dumper(@digit);  
+    chomp $line;
+    my $pattern = join "|", keys %val_store;
+    my @digit;
+    while ($line =~ /(?=($pattern|\d))/g) {
+    push @digit, looks_like_number($1) ? $1 : $val_store{$1};
+    }
     if (@digit) {
-	    if (scalar(@digit) > 1) 
-	    {   
-		$digit[0] = $val_store{$digit[0]} unless looks_like_number($digit[0]);
-		$digit[1] = $val_store{$digit[1]} unless looks_like_number($digit[1]);
-		$sum = $sum + int($digit[0].$digit[-1]); 
-	      }
-	    else
-	    {
-		$digit[0] = $val_store{$digit[0]} unless looks_like_number($digit[0]);
-		$sum = $sum + int($digit[0].$digit[0]);
-	    } 
+        if (scalar(@digit) > 1) 
+        {   
+    	$sum = $sum + int($digit[0].$digit[-1]); 
+          }
+        else
+        {
+    	$sum = $sum + int($digit[0].$digit[0]);
+        } 
     }
 }
 close($file);
